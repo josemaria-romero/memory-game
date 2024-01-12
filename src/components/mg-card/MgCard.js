8cd55869b7c70378f28ffaaad51f8ab327e00644
José María <josemaria.romero@sngular.com>
2024-01-12 11:07:37 +0100

    Create the cards in the game page

--- src/components/mg-card/MgCard.js
+++ src/components/mg-card/MgCard.js
@@ -1,15 +1,39 @@
 import { LitElement, html, css } from 'lit';
 
 export class MgCard extends LitElement {
+    static get properties() {
+        return {
+            number: { type: Number }
+        };
+    }
+
     static styles = [
         css`
             :host {
                 display: block;
             }
+
+            .mg-card {
+                border: 1px solid;
+                width: 100px;
+                height: 100px;
+                display: flex;
+                justify-content:center;
+                align-items: center;
+            }
+
         `
     ];
 
+    constructor() {
+        super();
+        this.number = 0;
+    }
+
     render() {
-        return html`<h1>Card</h1>`;
+        return html`
+            <div class='mg-card'>${this.number}</div>
+        `;
     }
 }
+
--- src/pages/mg-game/MgGame.js
+++ src/pages/mg-game/MgGame.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'lit';
+import "../../components/mg-card/mg-card.js";
 
 export class MgGame extends LitElement {
     static styles = [
@@ -6,10 +7,43 @@ export class MgGame extends LitElement {
             :host {
                 display: block;
             }
+
+            .cards-container{
+                margin: 0 auto;
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+                justify-content:center;
+                gap: 10px;
+            }
+
+            mg-card{
+                width: 100px;
+                height: 100px;
+            }
         `
     ];
 
+    static properties = {
+        cards: { type: Array },
+    }
+
+    constructor(){
+        super();
+        this.cards = Array.from({ length: 9 }, (_, index) => ({ number: index + 1 }));
+    }
+    
     render() {
-        return html`<h1>Game</h1>`;
+        return html`
+        <div class='cards-container'>
+            ${this.cards.map(
+                card =>
+                html`
+                    <mg-card .number=${card.number}></mg-card>
+                `
+            )}
+        </div>
+        
+        `;
     }
 }
