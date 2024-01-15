c2931ab6b771c3a5fc9b0471f048fa0a57415537
José María <josemaria.romero@sngular.com>
2024-01-15 16:08:06 +0100

    Formate MgCard

--- src/components/mg-card/MgCard.js
+++ src/components/mg-card/MgCard.js
@@ -1,64 +1,63 @@
-import { LitElement, html, css } from 'lit';
-import {classMap} from 'lit/directives/class-map.js';
-
+import { LitElement, html, css } from "lit";
+import { classMap } from "lit/directives/class-map.js";
 
 export class MgCard extends LitElement {
-    
-    static get properties() {
-        return {
-            tapped: { type: Boolean },
-            correct: { type: Boolean }
-        };
+  static get properties() {
+    return {
+      tapped: { type: Boolean },
+      correct: { type: Boolean },
+    };
+  }
+
+  static styles = css`
+    :host {
+      border: 1px solid;
+      width: 100px;
+      height: 100px;
     }
 
-    static styles = css`
-        :host {
-            border: 1px solid;
-            width: 100px;
-            height: 100px;
-        }
+    div {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
 
-        div{
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content:center;
-            align-items: center;
-        }
+    .tapped {
+      background-color: #000;
+    }
 
-        .tapped {
-            background-color: #000;
-        }
+    .untapped {
+      background-color: #fff;
+    }
 
-        .untapped {
-            background-color: #fff;
-        }
+    .correct {
+      background-color: green;
+    }
 
-        .correct {
-            background-color: green;
-        }
+    .wrong {
+      background-color: red;
+    }
+  `;
 
-        .wrong {
-            background-color: red;
-        }
+  constructor() {
+    super();
+    this.tapped = false;
+    this.correct = false;
+    this.addEventListener("click", () => {
+      this.tapped = !this.tapped;
+    });
+  }
+
+  render() {
+    const classes = { tapped: this.tapped, correct: this.correct };
+    this.classList = classMap(classes);
+
+    return html`
+      <div class=${classMap(classes)}>
+        <slot></slot>
+      </div>
     `;
-
-    constructor() {
-        super();
-        this.tapped = false;
-        this.correct = false;
-        this.addEventListener('click', () => { this.tapped = !this.tapped; });
-    }
-
-    render() {
-        const classes = { tapped: this.tapped, correct: this.correct };
-        this.classList=classMap(classes);
-
-        return html`
-            <div class=${classMap(classes)} >
-                <slot></slot>
-            </div>
-        `;
-    }
+  }
 }
-
